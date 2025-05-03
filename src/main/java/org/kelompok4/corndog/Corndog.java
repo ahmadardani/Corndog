94edd686bbc1de322e3709dca13a747019a1cf65
ahmadardani <ahmadardani2011@gmail.com>
2025-05-03 11:25:39 +0800

    Menambahkan .gitignore untuk proyek Java dan Membuat Login Form

---
+++ src/main/java/org/kelompok4/corndog/Corndog.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.kelompok4.corndog;
+
+/**
+ *
+ * @author akasaka
+ */
+public class Corndog {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
