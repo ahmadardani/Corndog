ef269e910f88f1297c055e4503cd2ca5eef3b0df
ahmadardani <ahmadardani2011@gmail.com>
2025-05-25 17:26:47 +0800

    menambah database yang baru

--- database/corndog.sql
+++ database/corndog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 23 Bulan Mei 2025 pada 11.36
+-- Waktu pembuatan: 25 Bulan Mei 2025 pada 11.25
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -24,26 +24,44 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `history`
+-- Struktur dari tabel `orders`
 --
 
-CREATE TABLE `history` (
+CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL,
-  `total` int(11) NOT NULL,
-  `order_date` datetime NOT NULL DEFAULT current_timestamp()
+  `total` int(11) DEFAULT NULL,
+  `order_date` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `history`
+-- Dumping data untuk tabel `orders`
 --
 
-INSERT INTO `history` (`order_id`, `total`, `order_date`) VALUES
-(15, 40000, '2025-05-22 14:35:14'),
-(16, 8000, '2025-05-22 14:36:20'),
-(17, 8000, '2025-05-22 14:36:34'),
-(18, 10000, '2025-05-22 15:21:25'),
-(19, 16000, '2025-05-22 15:38:27'),
-(20, 10000, '2025-05-22 15:44:58');
+INSERT INTO `orders` (`order_id`, `total`, `order_date`) VALUES
+(7, 25000, '2025-05-25 17:03:31');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `detail_id` int(11) NOT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `product_id` varchar(20) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `subtotal` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `order_details`
+--
+
+INSERT INTO `order_details` (`detail_id`, `order_id`, `product_id`, `quantity`, `subtotal`) VALUES
+(7, 7, 'FOOD001', 1, 10000),
+(8, 7, 'FOOD003', 1, 12000),
+(9, 7, 'FOOD002', 1, 3000);
 
 -- --------------------------------------------------------
 
@@ -53,18 +71,19 @@ INSERT INTO `history` (`order_id`, `total`, `order_date`) VALUES
 
 CREATE TABLE `products` (
   `product_id` varchar(20) NOT NULL,
-  `product_name` varchar(255) NOT NULL,
-  `harga` int(11) DEFAULT NULL,
-  `stok` int(11) NOT NULL
+  `product_name` varchar(255) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `products`
 --
 
-INSERT INTO `products` (`product_id`, `product_name`, `harga`, `stok`) VALUES
-('FOOD001', 'Corndog', 5000, 6),
-('FOOD002', 'Corndog Jumbo', 8000, 6);
+INSERT INTO `products` (`product_id`, `product_name`, `price`, `stock`) VALUES
+('FOOD001', 'Corndog Full Sosis', 10000, 9),
+('FOOD002', 'Corndog Mini Size', 3000, 9),
+('FOOD003', 'Corndog Mix Sosis & Mozzarella', 12000, 9);
 
 -- --------------------------------------------------------
 
@@ -74,9 +93,9 @@ INSERT INTO `products` (`product_id`, `product_name`, `harga`, `stok`) VALUES
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -85,17 +104,31 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`) VALUES
-(1, 'admin', 'admin@gmail.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '2025-05-09 13:26:00');
+(1, 'admin', 'admin@gmail.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '2025-05-24 06:49:17');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `history`
+-- Indeks untuk tabel `orders`
 --
-ALTER TABLE `history`
+ALTER TABLE `orders`
   ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indeks untuk tabel `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`detail_id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indeks untuk tabel `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
 
 --
 -- Indeks untuk tabel `users`
@@ -110,16 +143,33 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT untuk tabel `history`
+-- AUTO_INCREMENT untuk tabel `orders`
 --
-ALTER TABLE `history`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `order_details`
+--
+ALTER TABLE `order_details`
+  MODIFY `detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
